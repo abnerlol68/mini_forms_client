b2364b3872c651dc92e91f8c7490ce9f3d076770
JorshBG <jordiyair29@gmail.com>
2022-11-23 17:13:44 -0600

    Fix uri

--- src/View/Login.php
+++ src/View/Login.php
@@ -56,8 +56,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-    <link rel="stylesheet" href="src/css/login.css">
-    <link rel="stylesheet" href="src/libs/icons/font/typicons.css">
+    <link rel="stylesheet" href="<?= URL . 'src/css/login.css'?>">
+    <link rel="stylesheet" href="<?= URL . 'src/libs/icons/font/typicons.css' ?>">
 </head>
 <body>
   <p id="url" style="display: none;"><?= URL ?></p>
